977253a0bd6a8bf2de45b4f3907b8a2daf2e2bb0
Riyas Aboobaker <riyas.almoe@gmail.com>
2021-05-20 10:24:45 +0400

    New chapters added

---
+++ WPFCustomUserControlDependencyProperties/CustomUserControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCustomUserControlDependencyProperties
+{
+    /// <summary>
+    /// Interaction logic for CustomUserControl.xaml
+    /// </summary>
+    public partial class CustomUserControl : UserControl
+    {
+
+
+        public object Secret
+        {
+            get { return (object)GetValue(SecretProperty); }
+            set { SetValue(SecretProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Secret.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SecretProperty =
+            DependencyProperty.Register("Secret", typeof(object), typeof(CustomUserControl), new PropertyMetadata("Secret content"));
+
+
+        public CustomUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (spoilerGrid.Visibility == Visibility.Visible)
+            {
+                contentGrid.Visibility = Visibility.Visible;
+                spoilerGrid.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                contentGrid.Visibility = Visibility.Collapsed;
+                spoilerGrid.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ WPFWaremarkPlaceholderTextboxPart1/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFWaremarkPlaceholderTextboxPart1.Converters
+{
+    public class BoolToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool hasText = !(bool)values[0];
+            bool hasFocus = (bool)values[1];
+
+            if (hasText || hasFocus)
+            {
+                return Visibility.Collapsed; //instead of hidden to avoid interference with some focus properties
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
